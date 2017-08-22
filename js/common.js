747ff350428cb828d32975872c87333c443069ed
RatiboR1978 <klov29@mail.ru>
2017-08-22 14:38:35 +0700

    Поправил кнопку play

--- js/common.js
+++ js/common.js
@@ -4,11 +4,11 @@ $(function () {
     pauseButton.bind('click', function() {
         if (video.get(0).paused) {
             video.get(0).play();
-            $('.header__play').css({'background': 'url(../img/pause.png) no-repeat'});
+            $('.header__play').css({'background': 'url(img/pause.png) no-repeat'});
 
         } else {
             video.get(0).pause();
-            $('.header__play').css({'background': 'url(../img/play.png) no-repeat'});
+            $('.header__play').css({'background': 'url(img/play.png) no-repeat'});
         }
     });
 
